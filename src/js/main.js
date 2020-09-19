eb6ddd72dbd8211ced0ce6b861093d7567250902
Zhantore <753gg@mail.ru>
2020-09-19 18:24:53 +0600

    Методы для работы с элементами

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,11 @@
 import $ from './lib/lib';
 
 $('button').on('click', function () {
-    $(this).addAttr('data-attr', '123').getAttr('data-attr');
+    $('div').eq(1).toggleClass('active');
 });
+
+$('div').click(function () {
+    console.log($(this).index());
+});
+
+console.log($('div').eq(2).find('.some'));
